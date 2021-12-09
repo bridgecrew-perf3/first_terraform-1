5db07a27ee926fad82ede3c894c73ba36859f883
Simon Garcia J <sgarsa@gmail.com>
2021-12-09 12:08:51 -0800

    Update main.tf
    flipped to azure resource.

--- main.tf
+++ main.tf
@@ -1,16 +1,32 @@
-resource "google_compute_instance" "default" {
-  name         = "my-first-instance"
-  machine_type = "f1-micro"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-  }
-
+data "azurerm_resource_group" "main" {
+name = "${var.prefix}-resources"
 }
+
+data "azurerm_network_interface" "main" {
+name                = "${var.prefix}-nic1"
+resource_group_name = data.azurerm_resource_group.main.name
+}
+
+resource "azurerm_linux_virtual_machine" "default" {
+   name                  = "my-first-instance"
+   location              = data.azurerm_resource_group.main.location
+   resource_group_name   = data.azurerm_resource_group.main.name
+   network_interface_ids = [data.azurerm_network_interface.main.id]
+   size                  = "Standard_B1s"
+
+   os_disk {
+      caching              = "None"
+      storage_account_type = "Standard_LRS"
+   }
+
+   source_image_reference {
+      publisher = "Canonical"
+      offer     = "UbuntuServer"
+      sku       = "18.04-LTS"
+      version   = "latest"
+   }
+
+   admin_username                  = "azureuser"
+   admin_password                  = "Arct!q!!"
+   disable_password_authentication = false
+   }
