485a4f56bf33d2f5255f875b53f427e6b024689e
Simon Garcia J <sgarsa@gmail.com>
2021-12-31 13:47:53 -0800

    updated provider and vars reference

--- variables.tf
+++ variables.tf
@@ -1,15 +1,6 @@
    variable "region" {
       description = "AWS Region"
    }
-
-   variable "AWS_ACCESS_KEY_ID" {
-      description = "AWS Access Key"
-   }
-
-   variable "AWS_SECRET_ACCESS_KEY" {
-      description = "AWS Secret Key"
-   }
-
    variable "student_id" {
       description = "Workshop Student ID "
    }
