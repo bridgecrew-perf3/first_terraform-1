3a06723f9431c1cdb4c53baba6655886e06c4f6e
Simon Garcia J <sgarsa@gmail.com>
2021-12-31 13:36:50 -0800

    updated vars file

--- variables.tf
+++ variables.tf
@@ -2,14 +2,14 @@
       description = "AWS Region"
    }
 
-   variable "access_key"
+   variable "access_key" {
       description = "AWS Access Key"
    }
 
-   variable "secret_key"
+   variable "secret_key" {
       description = "AWS Secret Key"
    }
 
-   variable "student_id"
+   variable "student_id" {
       description = "Workshop Student ID"
    }
